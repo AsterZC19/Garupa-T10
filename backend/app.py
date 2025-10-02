f418a7e95661c5d36e2b0ce6876933d4a68b3611
AsterZC19 <1669687633zc19@gmail.com>
2025-10-02 13:33:42 +0800

    fix: refresh bug

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,8 @@ from scheduler import init_scheduler
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-app = Flask(__name__, static_folder='../frontend/dist', static_url_path='/')
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__, static_folder=os.path.join(basedir, '..', 'frontend', 'dist'), static_url_path='/')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_file = os.path.join(basedir, 'data.db')
